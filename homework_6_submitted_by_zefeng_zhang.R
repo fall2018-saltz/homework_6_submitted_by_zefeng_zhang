6e19fb96902643224aff420f8f4020a2aa7b5f71
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 21:28:13 -0400

    Saving homework_6_submitted_by_zefeng_zhang.R

--- homework_6_submitted_by_zefeng_zhang.R
+++ homework_6_submitted_by_zefeng_zhang.R
@@ -27,4 +27,5 @@ arrests$stateName <- row.names(arrests)
 totalData<-merge (cleanCensus,arrests, by=c("stateName"))
 
 #Step B: Explore the Data – Understanding distributions
-#Create a histogram using GGPLOT for the population a different histogram for the murder rate
+#Create a histogram using GGPLOT for the population
+# and different histogram for the murder rate
