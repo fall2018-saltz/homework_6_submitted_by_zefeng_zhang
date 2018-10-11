afa76b4b506ec17ba9789c93c73faaba786995d1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 12:43:22 -0400

    Saving homework_6_submitted_by_zefeng_zhang.R

--- homework_6_submitted_by_zefeng_zhang.R
+++ homework_6_submitted_by_zefeng_zhang.R
@@ -42,10 +42,10 @@ hist2<- ggplot(totalData,aes(x=Murder)) +
 hist3 <-ggplot(totalData, aes(x=Assault))+
   geom_histogram (bins=20, color ="blue",fill="white")
 #create a histogram for the uraban population rate
-hist4<-ggplot(totalData,aes(x=UrbanPop)) +
+hist4 <-ggplot(totalData,aes(x=UrbanPop)) +
   geom_histogram(bins=50, color ="blue",fill="white")
 #create a histogram for the rape rate
-hist5<-ggplot(totalData, aes(x=Rape))+
+hist5 <-ggplot(totalData, aes(x=Rape))+
   geom_histogram (bins=20, color ="blue",fill="white")
 #I explored multiple bin widths and tried to find the best to illustrate the data.
         
@@ -67,28 +67,28 @@ str(totalData$murderPerState)
 
 #8)	Generate a bar chart, with the number of murders per state
  #Hint: use the geom_col function
-ggplot(totalData, aes(x=stateName, y=murderPerState))+
+barchart1 <- ggplot(totalData, aes(x=stateName, y=murderPerState))+
 geom_col()
 
 #9)	Generate a bar chart, with the number of murders per state. 
 #Rotate text (on the X axis), so we can see x labels, also add a title named “Total Murders”.
-ggplot(totalData, aes(x=stateName, y=murderPerState))+
+barchart2 <-ggplot(totalData, aes(x=stateName, y=murderPerState))+
   geom_col()+
   theme(axis.text.x =element_text(angle=90,hjust=1)) + ggtitle ("Total Murders")
 # 10) Generate a new bar chart, the same as in the previous step, but also sort the x-axis by the murder rate
-ggplot(totalData, aes(x=reorder(stateName,murderPerState), y=murderPerState))+
+barchart3 <-ggplot(totalData, aes(x=reorder(stateName,murderPerState), y=murderPerState))+
   geom_col()+
   theme(axis.text.x =element_text(angle=90,hjust=1)) + ggtitle ("Total Murders")
 
 # 11) Generate a third bar chart, the same as the previous step, but also showing percentOver18 as the color of the bar
-ggplot(totalData, aes(x=reorder(stateName,murderPerState), y=murderPerState, fill = percentOver18))+
+barchart4 <-ggplot(totalData, aes(x=reorder(stateName,murderPerState), y=murderPerState, fill = percentOver18))+
   geom_col()+
   theme(axis.text.x =element_text(angle=90,hjust=1)) + ggtitle ("Total Murders")
 
 #Step D: Explore Murders – scatter chart
 #12)	Generate a scatter plot – have population on the X axis, the percent over 18 on the y axis
 #and the size & color represent the murder rate
-ggplot(totalData, aes(x = population , y =percentOver18 , size = murderPerState , col =murderPerState)) + 
+scatterplot<- ggplot(totalData, aes(x = population , y =percentOver18 , size = murderPerState , col =murderPerState)) + 
   geom_point()
 
   
